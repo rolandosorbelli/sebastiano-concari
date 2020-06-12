4f2d3f65299a989d741c15d8dc719bcd4840fb95
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2020-06-12 10:48:39 +0100

    Adjusted icons in hamburger menu

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,9 +2,24 @@ import React from "react"
 
 import logo from "../../images/logo.svg"
 import skull from "../../images/skull.svg"
-import { Menu } from "@material-ui/icons"
+import home from "../../images/home.svg"
+import art from "../../images/art.svg"
+import contact from "../../images/contact.svg"
+import instagram from "../../images/instagram.svg"
+import shop from "../../images/shop.svg"
+import tattoos from "../../images/tattoos.svg"
+
+import { Menu, Close } from "@material-ui/icons"
 
 const Navbar = () => {
+  const navArray = {
+    Home: home,
+    Tattoos: tattoos,
+    Art: art,
+    Shop: shop,
+    "Contact me": contact,
+    Instagram: instagram,
+  }
   return (
     <nav>
       <div className="content">
@@ -22,12 +37,19 @@ const Navbar = () => {
         <Menu className="menu" />
       </div>
       <div className="dropdown">
-        <a href="/">Home</a>
-        <a href="/">Tatoos</a>
-        <a href="/">Art</a>
-        <a href="/">Shop</a>
-        <a href="mailto:rolando.sorbelli@gmail.com">Contact me</a>
-        <a href="/">Instagram</a>
+        <div className="content">
+          <div className="close">
+            <Close />
+          </div>
+          {Object.entries(navArray).map(([key, value], i) => (
+            <a href="/" key={i}>
+              <span className="circle">
+                <img src={value} alt="" />
+              </span>
+              <span>{key}</span>
+            </a>
+          ))}
+        </div>
       </div>
     </nav>
   )
