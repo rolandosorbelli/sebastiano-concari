213b887110567aa3d957f29825511a83b0ab51ef
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2020-06-14 13:10:11 +0100

    Added cta-shop

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Hero from "../components/Hero"
 
 import tattoos from "../images/cta-tattoos.jpg"
 import art from "../images/cta-art.jpg"
+import shop from "../images/cta-shop.jpg"
 
 const Home = () => (
   <Layout>
@@ -21,6 +22,12 @@ const Home = () => (
       heading="My art"
       description="Not only interested in tattoos? Have a look at the art I make here!"
       url="/art"
+    />
+    <Hero
+      image={shop}
+      heading="My shop"
+      description="Browse my shop on Redbubble"
+      url="/shop"
     />
   </Layout>
 )
