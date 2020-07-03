c1e3dde92a49787e2977f7eb0425182dbb441d11
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2020-07-03 16:23:59 +0100

    Tweaked categories

--- src/pages/art.js
+++ src/pages/art.js
@@ -3,17 +3,12 @@ import React from "react"
 import Layout from "../components/layout"
 import Navbar from "../components/Navbar"
 import Header from "../components/Header"
-import Categories from "../components/Categories"
 
-const Art = ({ data }) => {
-  const categories = data.allContentfulTattooCategory.edges
-  return (
-    <Layout>
-      <Navbar />
-      <Header title="Art" />
-      <Categories content={categories} />
-    </Layout>
-  )
-}
+const Art = () => (
+  <Layout>
+    <Navbar />
+    <Header title="Art" />
+  </Layout>
+)
 
 export default Art
