9d857f0f5b25302b5895d0e22c20341941a0b56b
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2020-07-04 23:57:28 +0100

    Configured Art section with Contentful content

--- src/pages/art.js
+++ src/pages/art.js
@@ -1,14 +1,61 @@
 import React from "react"
+import { graphql } from "gatsby"
+import SimpleReactLightbox from "simple-react-lightbox"
 
 import Layout from "../components/layout"
 import Navbar from "../components/Navbar"
 import Header from "../components/Header"
+import Gallery from "../components/Gallery"
+import Footer from "../components/Footer"
 
-const Art = () => (
-  <Layout>
-    <Navbar />
-    <Header title="Art" />
-  </Layout>
-)
+const Art = ({ data }) => {
+  const categories = data.allContentfulArtCategory.edges
+  const grid = data.allContentfulArt.edges
+
+  return (
+    <Layout>
+      <Navbar />
+      <Header title="Art" />
+      <SimpleReactLightbox>
+        <Gallery content={grid} categories={categories} />
+      </SimpleReactLightbox>
+      <Footer />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allContentfulArt(sort: { fields: createdAt, order: DESC }) {
+      edges {
+        node {
+          alt
+          category {
+            slug
+            title
+          }
+          description
+          image {
+            description
+            fluid(maxWidth: 1200, quality: 85) {
+              src
+              ...GatsbyContentfulFluid
+            }
+          }
+          slug
+          title
+        }
+      }
+    }
+    allContentfulArtCategory(sort: { fields: title, order: ASC }) {
+      edges {
+        node {
+          title
+          slug
+        }
+      }
+    }
+  }
+`
 
 export default Art
