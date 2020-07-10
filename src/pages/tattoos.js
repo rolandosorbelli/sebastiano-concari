a6d898e686b605bb006b1a59bfdce47c6dc97767
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2020-07-10 15:59:48 +0100

    Tweaked alt in tattoos and art

--- src/pages/tattoos.js
+++ src/pages/tattoos.js
@@ -29,7 +29,6 @@ export const query = graphql`
     allContentfulTattoo(sort: { fields: createdAt, order: DESC }) {
       edges {
         node {
-          alt
           category {
             slug
             title
